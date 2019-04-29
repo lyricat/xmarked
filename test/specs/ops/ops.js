fd80b58fcb415693b9b959d104a0bf64493147b4
Wei <5h3ll3x@gmail.com>
2019-04-29 14:33:22 +0800

    better footnotes

--- test/specs/ops/ops.js
+++ test/specs/ops/ops.js
@@ -8,9 +8,9 @@ var testcases = [
   {
     "section": "Footnotes",
     "html": '<p>Here&#39;s a simple footnote,<a id="footnote-1" href="#footnote-item-1" title="1"><sup>1</sup></a> and here&#39;s a longer one<a id="footnote-bignote" href="#footnote-item-bignote" title="bignote"><sup>2</sup></a>.</p>\n'
-            + '<ol class="footnotes"><li class="footnote-item" id="footnote-item-1"><a href="#footnote-1" title="Jump to">^</a> This is the first footnote.</li>\n'
+            + '<ol class="footnotes"><li class="footnote-item" id="footnote-item-1"><a href="#footnote-1" title="Jump to">^</a> This is the <strong>FIRST</strong> footnote.</li>\n'
             + '<li class="footnote-item" id="footnote-item-bignote"><a href="#footnote-bignote" title="Jump to">^</a> Here&#39;s another</li>\n</ol>\n',
-    "markdown": "Here's a simple footnote,[^1] and here's a longer one[^bignote].\n\n[^1]: This is the first footnote.\n[^bignote]: Here's another",
+    "markdown": "Here's a simple footnote,[^1] and here's a longer one[^bignote].\n\n[^1]: This is the **FIRST** footnote.\n[^bignote]: Here's another",
     "example": 1901
   },
 ]
