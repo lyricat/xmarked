3e5bb1a3876cc03dcc13ab29cb80daa91d8146da
Wei <5h3ll3x@gmail.com>
2019-04-29 12:29:15 +0800

    footnotes

--- test/specs/ops/ops.js
+++ test/specs/ops/ops.js
@@ -7,9 +7,9 @@ var testcases = [
   },
   {
     "section": "Footnotes",
-    "html": '<p>Here&#39;s a simple footnote,<a id="footnote-1" href="#footnote-block-1" title="1">1</a> and here&#39;s a longer one<a id="footnote-bignote" href="#footnote-block-bignote" title="bignote">bignote</a>.</p>\n'
-            + '<p id="footnote-block-1"><a href="#footnote-1" title="Jump to">^</a>This is the first footnote.</p>\n'
-            + '<p id="footnote-block-bignote"><a href="#footnote-bignote" title="Jump to">^</a>Here&#39;s another</p>\n',
+    "html": '<p>Here&#39;s a simple footnote,<a id="footnote-1" href="#footnote-item-1" title="1">1</a> and here&#39;s a longer one<a id="footnote-bignote" href="#footnote-item-bignote" title="bignote">bignote</a>.</p>\n'
+            + '<p class="footnote-item" id="footnote-item-1"><span><code>1.</code></span><a href="#footnote-1" title="Jump to">^</a>This is the first footnote.</p>\n'
+            + '<p class="footnote-item" id="footnote-item-bignote"><span><code>2.</code></span><a href="#footnote-bignote" title="Jump to">^</a>Here&#39;s another</p>\n',
     "markdown": "Here's a simple footnote,[^1] and here's a longer one[^bignote].\n\n[^1]: This is the first footnote.\n\n[^bignote]: Here's another",
     "example": 1901
   },
