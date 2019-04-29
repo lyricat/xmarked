ebfde63017369f659f37a128782f56beda704b60
Wei <5h3ll3x@gmail.com>
2019-04-29 12:18:15 +0800

    footnotes

--- test/specs/ops/ops.js
+++ test/specs/ops/ops.js
@@ -4,7 +4,15 @@ var testcases = [
     "html": "<div class='message-block is-note'>\n<p class='message-title'><span class='message-status'></span>NOTE</p>\n<p>This is a NOTE</p>\n</div>",
     "markdown": "> [!NOTE]\n> This is a NOTE",
     "example": 1900
-  }
+  },
+  {
+    "section": "Footnotes",
+    "html": '<p>Here&#39;s a simple footnote,<a id="footnote-1" href="#footnote-block-1" title="1">1</a> and here&#39;s a longer one<a id="footnote-bignote" href="#footnote-block-bignote" title="bignote">bignote</a>.</p>\n'
+            + '<p id="footnote-block-1"><a href="#footnote-1" title="Jump to">^</a>This is the first footnote.</p>\n'
+            + '<p id="footnote-block-bignote"><a href="#footnote-bignote" title="Jump to">^</a>Here&#39;s another</p>\n',
+    "markdown": "Here's a simple footnote,[^1] and here's a longer one[^bignote].\n\n[^1]: This is the first footnote.\n\n[^bignote]: Here's another",
+    "example": 1901
+  },
 ]
 
 var marked = require('../../../lib/marked.js')
